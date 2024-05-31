1b94b7c552fc580bee200e5ccc0186e0c5940412
CharaD7 <jijakahn6@gmail.com>
2024-05-31 19:48:44 +0100

    Removed project rule

---
+++ src/app/index.tsx
@@ -0,0 +1,34 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const polls = [1, 2, 4];
+
+export default function HomeScreen() {
+  return (
+    <FlatList
+      data={polls}
+      contentContainerStyle={styles.container}
+      renderItem={() => (
+        <View style={styles.pollContainer}>
+          <Text style={styles.pollTitle}>Example Poll Question</Text>
+        </View>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    gap: 5,
+  },
+  pollContainer: {
+    backgroundColor: 'lightblue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  pollTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
