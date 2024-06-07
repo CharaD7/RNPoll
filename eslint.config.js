9f9b4282ce2260919d04ca6ebd16cd378e1073bf
CharaD7 <jijakahn6@gmail.com>
2024-06-07 08:16:24 +0000

    Removed prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
+//import airbnb from '@typescript-eslint/eslint-plugin';
 import globals from 'globals';
+import js from '@eslint/js';
 
 export default [
+  //airbnb.hooks,
+  //airbnb.typescript,
+  js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 2022,
