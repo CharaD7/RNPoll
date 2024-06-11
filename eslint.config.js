f4eea26f85cdc1d4b003734e3b1ff0f7618b2778
CharaD7 <jijakahn6@gmail.com>
2024-06-11 21:13:43 +0000

    Added ignore

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,9 @@ import js from '@eslint/js';
 export default [
   js.configs.recommended,
   {
-    ignores: ['!.*', '*.js', 'dist', 'node_modules'],
+    ignores: ['!.*', '**/*.js', 'dist', 'node_modules'],
+  },
+  {
     languageOptions: {
       ecmaVersion: 2022,
       parserOptions: {
