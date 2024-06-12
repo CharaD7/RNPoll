2e623c8f1a2b03781357ce8bf9a3802e41e2cf60
CharaD7 <jijakahn6@gmail.com>
2024-06-12 18:00:19 +0000

    Renamed babel config, maintained config setup

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,18 +1,6 @@
-// module.exports = function (api) {
-//   api.cache(true);
-//   return {
-//     presets: ['babel-preset-expo'],
-//   };
-// };
-
-module.exports = {
-  presets: [
-    // 'babel-preset-expo',
-    '@babel/preset-typescript',
-    {
-      targets: {
-        node: 'current'
-      }
-    }
-  ]
-}
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
